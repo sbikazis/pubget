cd67c005aafebffe104d74e6ced3c2d82c84bbf8
Zakaria Sbika <zakariasbika253@gmail.com>
2026-03-20 23:49:28 +0000

    changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services)
+    id("com.google.gms.google-services") // 🔥 مهم
 }
 
 android {
@@ -21,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.pubget"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -33,8 +29,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -42,4 +36,12 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // 🔥 Firebase BOM
+    implementation(platform("com.google.firebase:firebase-bom:34.11.0"))
+
+    // 🔥 Firebase الأساسي (يمكنك إضافة المزيد لاحقًا)
+    implementation("com.google.firebase:firebase-analytics")
 }
