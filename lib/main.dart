ca323b77469cf8e1fc0bb348e1e149d1085864cc
Zakaria Sbika <zakariasbika253@gmail.com>
2026-03-24 19:37:11 +0100

    befor last

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,21 @@
 // Flutter
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-// Firebase
-import 'package:firebase_core/firebase_core.dart';
 
-// Google Mobile Ads
-import 'package:google_mobile_ads/google_mobile_ads.dart';
-
-import 'package:pubget/app.dart';
-
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  try {
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: 'AIzaSyDPgjnaviwqxz8lGK_o9pjg4zuoB7RrVBw',
-        appId: '1:193460577475:android:2ddc9727c0ca84dbb86e76',
-        messagingSenderId: '193460577475',
-        projectId: 'pubget-817cf',
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.dumpErrorToConsole(details);
+  };
+
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("APP STARTED"),
+        ),
       ),
-    );
-  } catch (e) {
-    debugPrint("🔥 Firebase init error: $e");
-  }
-
-  try {
-    await MobileAds.instance.initialize();
-  } catch (e) {
-    debugPrint("🔥 Ads init error: $e");
-  }
-
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]);
-
-  runApp(const PubgetApp());
+    ),
+  );
 }
