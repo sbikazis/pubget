aa1a630b082e450e40cd649c85c1f0574b1f27bc
Zakaria Sbika <zakariasbika253@gmail.com>
2026-03-26 23:28:20 +0100

    fix namespace the true

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pubget"
+    namespace = "com.pytrozis.pubget"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
